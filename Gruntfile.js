3335864e017e89cccf39919b0d4652643b16b5ca
Sam Gruber <grubermensch@gmail.com>
2015-06-13 11:32:16 -0700

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function (grunt)
+{
+  // Project configuration.
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          hostname: 'localhost',
+          port: 7997,
+          base: 'build'
+        }
+      }
+    },
+
+    watch: {
+      jsx: {
+        files: ['source/javascript/**/*.jsx'],
+        tasks: ['browserify']
+      },
+      html: {
+        files: ['source/index.html'],
+        tasks: ['copy:index']
+      }
+    },
+
+    browserify: {
+      build: {
+        files: {
+          'build/main.js': ['source/javascript/main.jsx']
+        },
+        options: {
+          transform: ['reactify']
+        }
+      }
+    },
+
+    copy: {
+      index: {
+        nonull: true,
+        src: 'source/index.html',
+        dest: 'build/index.html'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('build', ['browserify', 'copy']);
+  grunt.registerTask('default', ['connect', 'build', 'watch']);
+};
