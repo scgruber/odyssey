ce058000b62ed945882a58a62dc87a08d000e049
Sam Gruber <grubermensch@gmail.com>
2015-06-13 14:27:58 -0700

    Watch regular JS files too

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function (grunt)
 
     watch: {
       jsx: {
-        files: ['source/javascript/**/*.jsx'],
+        files: ['source/javascript/**/*.jsx', 'source/javascript/**/*.js'],
         tasks: ['browserify']
       },
       html: {
